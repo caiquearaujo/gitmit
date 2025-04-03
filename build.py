7d6fbfee3e677b8baac35ae2a4540db73383cc9b
Caique Araujo <caique@piggly.com.br>
2025-04-02 23:47:44 -0300

    ♻️  improvement(v1.0.0): Get dynamic version on build time
    Change build process to get dynamic version using the __VERSION__ at build time. Also, the argparse usage was removed.

--- build.py
+++ build.py
@@ -3,30 +3,19 @@ import argparse
 import zipapp
 import tempfile
 
+from gitmit import __VERSION__
 from src.utils.terminal import display_info, display_success, display_warning
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="Create a zipapp with a version suffix."
-    )
-
-    parser.add_argument(
-        "-v",
-        "--version",
-        required=True,
-        help="Version of the build (eg: 1.0.0)",
-    )
-
-    args = parser.parse_args()
-
+    version = __VERSION__
     dist_dir = os.path.join(tempfile.gettempdir(), "gitmit")
 
     if not os.path.exists(dist_dir):
         os.makedirs(dist_dir)
         display_info(f"Directory '{dist_dir}' created.")
 
-    output_file = os.path.join(dist_dir, f"gitmit-{args.version}.pyz")
+    output_file = os.path.join(dist_dir, f"gitmit-{version}.pyz")
 
     zipapp.create_archive(
         source=".",
