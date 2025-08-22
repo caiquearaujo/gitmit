efd41bad2e0a1c1dc715753cf5f2661cca256ba5
Caique Araujo <caique@piggly.com.br>
2025-08-22 12:24:58 -0300

    ✨ feat(llms): Improve commit message generation prompts for better results.
    The prompts used to generate commit messages have been updated to provide more detailed instructions and context to the language model. This results in more accurate and relevant commit messages.

--- src/gitmit/resources/llms.py
+++ src/gitmit/resources/llms.py
@@ -60,29 +60,31 @@ def prompt_commit_from_resume(
         str: The generated commit message.
     """
     contents = [
-        "You are a helpful assistant that generates commit messages for changes on a git repository.",
-        "You should generate a commit message from the resume below and set the commit type based on it.",
-        "Please follow rules:",
-        "1. You must use an existing commit type from the CSV 'type' property below. To choose the commit type, you should see the resume below and choose the most appropriate commit type that covers the changes. See the 'meaning' property to understand the commit type;",
-        "2. If you can't choose a commit type or no commit type is suitable, use the 'other' commit type;",
-        "3. When answering, the 'scope' is a overall categorization for the resume. Not related to type. E.g: If main resume are about entity, the scope will be 'entities'. When is a version update, will be 'v1.0.0', etc;",
-        "4. When answering, the 'short_description' should resume the changes with less than 80 characters. It will be used as the title of the commit message; E.g: 'Add the FileEntity class';",
-        "5. When answering, the 'description' should be a detailed description of the resume with less than 500 characters. It will be used as the body of the commit message. E.g: 'This commit adds the FileEntity class to the project. This class is responsible for representing a file in the system. It contains the following properties: name, size, type, etc.';",
-        "6. When answering, the 'reason' should be a brief explanation of why you choose the commit type;",
-        "7. If provided, use the user short explanation to understand what changed;",
-        "8. Be careful with the commit type 'feat', you must use it ONLY main changes are about a new feature or a explicit feature addition. Otherwise, choose another commit type.",
-        ":>> available commit types",
+        "YOU ARE a helpful assistant with HIGH EXPERIENCE at software engineering.",
+        "YOUR GOAL is to generate a VERY SPECIFIC commit message for a git repository.",
+        "TO ACHIEVE YOUR GOAL you need to ANALYZE CAREFULLY the changes related below. Then, YOU SHOULD to write a message and categorize the changes and set the valid commit type to them.",
+        "YOU SHOULD STRICT FOLLOW THE RULES BELOW:",
+        "0. Modified files has HIGHER PRIORITY than new files (aka untracked files), UNLESS user explicitly says otherwise;",
+        "1. YOU MUST TO USE an existing commit type from the CSV 'type' property below. To choose the commit type, YOU SHOULD CATEGORIZE the changes and choose the most appropriate commit type that covers them. At CSV, see the 'meaning' property to understand the context of a commit type and categorize it accordingly;",
+        "2. IF YOU CANNOT CHOOSE a commit type or NO COMMIT TYPE IS SUITABLE, YOU SHOULD USE the 'other' commit type;",
+        "3. JSON 'scope' PROPERTY is a overall categorization for the changes. Not related to type. E.g: If main changes are about entity, the scope will be 'entities'. When is a version update, will be 'v1.0.0', etc. IF YOU CANNOT CHOOSE a scope, use 'other' scope;",
+        "4. JSON 'short_description' PROPERTY SHOULD resume changes with less than 80 characters. It will be used as the title of the commit message. IT MUST BE SIMPLE, CONCISE AND VERY MEANINGFUL. E.g: 'Add the FileEntity class';",
+        "5. JSON 'description' PROPERTY SHOULD BE a detailed description of the changes with less than 500 characters. It will be used as the body of the commit message. DO NOT REPEAT YOURSELF, IT MUST BE DETAILED AND VERY MEANINGFUL DESCRIPTION. E.g: 'This commit adds the FileEntity class to the project. This class is responsible for representing a file in the system. It contains the following properties: name, size, type, etc.';",
+        "6. JSON 'reason' PROPERTY SHOULD BE a bried explanation to user which explain what you have understood about the changes;",
+        "7. IF USER PROVIDE AN EXPLANATION ABOUT COMMIT, what he said will have HIGH PRIORITY and you SHOULD RELATE this information to the changes seen in the code;",
+        "8. BE CAREFUL WITH THE COMMIT TYPE 'feat', YOU MUST USE IT ONLY MAIN CHANGES ARE ABOUT A NEW FEATURE OR A EXPLICIT FEATURE ADDITION. OTHERWISE, CHOOSE ANOTHER COMMIT TYPE.",
+        ":>> AVAILABLE COMMIT TYPES",
         get_commit_types_resume(),
-        "<<: end of available commit types",
-        ":>> resume",
+        "<<: end of AVAILABLE COMMIT TYPES",
+        ":>> CHANGES",
         resume,
-        "<<: end of resume",
+        "<<: end of CHANGES",
     ]
 
     if explanation is not None:
-        contents.append(":>> user short explanation about what changed")
+        contents.append(":>> USER EXPLANATION")
         contents.append(explanation)
-        contents.append("<<: end of user short explanation about what changed")
+        contents.append("<<: end of USER EXPLANATION")
 
     return "\n".join(contents)
 
@@ -105,29 +107,30 @@ def prompt_commit_from_files(
         return None
 
     contents = [
-        "You are a helpful assistant that generates commit messages for changes on a git repository.",
-        "You should generate a commit message from the changes below and set the commit type based on the changes.",
-        "Please follow rules:",
-        "0. When analyzing the changes, new files (aka untracked files) has priority over modified files;",
-        "1. You must use an existing commit type from the CSV 'type' property below. To choose the commit type, you should see the changes and choose the most appropriate commit type that covers the changes. See the 'meaning' property to understand the commit type;",
-        "2. If you can't choose a commit type or no commit type is suitable, use the 'other' commit type;",
-        "3. When answering, the 'scope' is a overall categorization for the changes. Not related to type. E.g: If main changes are about entity, the scope will be 'entities'. When is a version update, will be 'v1.0.0', etc;",
-        "4. When answering, the 'short_description' should resume changes with less than 80 characters. It will be used as the title of the commit message; E.g: 'Add the FileEntity class';",
-        "5. When answering, the 'description' should be a detailed description of the changes with less than 500 characters. It will be used as the body of the commit message. E.g: 'This commit adds the FileEntity class to the project. This class is responsible for representing a file in the system. It contains the following properties: name, size, type, etc.';",
-        "6. When answering, the 'reason' should be a brief explanation of why you choose the commit type;",
-        "7. If provided, use the user short explanation to understand what changed;",
-        "8. Be careful with the commit type 'feat', you must use it ONLY main changes are about a new feature or a explicit feature addition. Otherwise, choose another commit type.",
-        ":>> available commit types",
+        "YOU ARE a helpful assistant with HIGH EXPERIENCE at software engineering.",
+        "YOUR GOAL is to generate a VERY SPECIFIC commit message for a git repository.",
+        "TO ACHIEVE YOUR GOAL you need to ANALYZE CAREFULLY the changes related below. Then, YOU SHOULD to write a message and categorize the changes and set the valid commit type to them.",
+        "YOU SHOULD STRICT FOLLOW THE RULES BELOW:",
+        "0. Modified files has HIGHER PRIORITY than new files (aka untracked files), UNLESS user explicitly says otherwise;",
+        "1. YOU MUST TO USE an existing commit type from the CSV 'type' property below. To choose the commit type, YOU SHOULD CATEGORIZE the changes and choose the most appropriate commit type that covers them. At CSV, see the 'meaning' property to understand the context of a commit type and categorize it accordingly;",
+        "2. IF YOU CANNOT CHOOSE a commit type or NO COMMIT TYPE IS SUITABLE, YOU SHOULD USE the 'other' commit type;",
+        "3. JSON 'scope' PROPERTY is a overall categorization for the changes. Not related to type. E.g: If main changes are about entity, the scope will be 'entities'. When is a version update, will be 'v1.0.0', etc. IF YOU CANNOT CHOOSE a scope, use 'other' scope;",
+        "4. JSON 'short_description' PROPERTY SHOULD resume changes with less than 80 characters. It will be used as the title of the commit message. IT MUST BE SIMPLE, CONCISE AND VERY MEANINGFUL. E.g: 'Add the FileEntity class';",
+        "5. JSON 'description' PROPERTY SHOULD BE a detailed description of the changes with less than 500 characters. It will be used as the body of the commit message. DO NOT REPEAT YOURSELF, IT MUST BE DETAILED AND VERY MEANINGFUL DESCRIPTION. E.g: 'This commit adds the FileEntity class to the project. This class is responsible for representing a file in the system. It contains the following properties: name, size, type, etc.';",
+        "6. JSON 'reason' PROPERTY SHOULD BE a bried explanation to user which explain what you have understood about the changes;",
+        "7. IF USER PROVIDE AN EXPLANATION ABOUT COMMIT, what he said will have HIGH PRIORITY and you SHOULD RELATE this information to the changes seen in the code;",
+        "8. BE CAREFUL WITH THE COMMIT TYPE 'feat', YOU MUST USE IT ONLY MAIN CHANGES ARE ABOUT A NEW FEATURE OR A EXPLICIT FEATURE ADDITION. OTHERWISE, CHOOSE ANOTHER COMMIT TYPE.",
+        ":>> AVAILABLE COMMIT TYPES",
         get_commit_types_resume(),
-        "<<: end of available commit types",
-        ":>> changes",
+        "<<: end of AVAILABLE COMMIT TYPES",
+        ":>> CHANGES",
         changes,
-        "<<: end of changes",
+        "<<: end of CHANGES",
     ]
 
     if explanation is not None:
-        contents.append(":>> user short explanation about what changed")
+        contents.append(":>> USER EXPLANATION")
         contents.append(explanation)
-        contents.append("<<: end of user short explanation about what changed")
+        contents.append("<<: end of USER EXPLANATION")
 
     return "\n".join(contents)
